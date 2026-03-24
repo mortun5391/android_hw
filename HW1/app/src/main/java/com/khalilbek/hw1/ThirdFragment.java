e12c989e8c3865a16c2b4710b1a3196d33f22f1b
Khalilbek <haxec4@mail.ru>
2026-03-24 03:36:16 +0300

    hw1

---
+++ HW1/app/src/main/java/com/khalilbek/hw1/ThirdFragment.java
@@ -0,0 +1,30 @@
+package com.khalilbek.hw1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class ThirdFragment extends Fragment {
+
+    public ThirdFragment() {
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_third, container, false);
+
+        WebView webView = view.findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.wikipedia.org/");
+
+        return view;
+    }
+}
